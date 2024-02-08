6d5b0cd23ae75c758c43a194db7cde961951fbea
ashutshm778 <ashutoshm778@gmail.com>
2024-02-08 18:25:43 +0530

    new update

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -4,6 +4,154 @@
     <div class="page-wrapper">
         <div class="page-content">
 
+            <div class="row row-cols-1 row-cols-md-2 row-cols-xl-4">
+                <div class="col">
+                    <div class="card radius-10 border-start border-0 border-4 border-info">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <div>
+                                    <p class="mb-0 text-secondary">Total Product</p>
+                                    <h4 class="my-1 text-info">0</h4>
+                                </div>
+                                <div class="widgets-icons-2 rounded-circle bg-gradient-blues text-white ms-auto"><i
+                                        class='bx bxs-cart'></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card radius-10 border-start border-0 border-4 border-danger">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <div>
+                                    <p class="mb-0 text-secondary">Total Orders</p>
+                                    <h4 class="my-1 text-danger">0</h4>
+                                </div>
+                                <div class="widgets-icons-2 rounded-circle bg-gradient-burning text-white ms-auto"><i
+                                        class='bx bxs-wallet'></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card radius-10 border-start border-0 border-4 border-warning">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <div>
+                                    <p class="mb-0 text-secondary">Total Vendors</p>
+                                    <h4 class="my-1 text-warning">0</h4>
+                                </div>
+                                <div class="widgets-icons-2 rounded-circle bg-gradient-orange text-white ms-auto"><i
+                                        class='bx bxs-group'></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card radius-10 border-start border-0 border-4 border-warning">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <div>
+                                    <p class="mb-0 text-secondary">Total Customers</p>
+                                    <h4 class="my-1 text-warning">0</h4>
+                                </div>
+                                <div class="widgets-icons-2 rounded-circle bg-gradient-orange text-white ms-auto"><i
+                                        class='bx bxs-group'></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <!--end row-->
+
+            <div class="row">
+                <div class="col-12 col-lg-12 d-flex">
+                    <div class="card radius-10 w-100">
+                        <div class="card-header">
+                            <div class="d-flex align-items-center">
+                                <div>
+                                    <h6 class="mb-0">Sales Overview</h6>
+                                </div>
+                                <div class="dropdown ms-auto">
+                                    <a class="dropdown-toggle dropdown-toggle-nocaret" href="#"
+                                        data-bs-toggle="dropdown"><i
+                                            class='bx bx-dots-horizontal-rounded font-22 text-option'></i>
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="javascript:;">Action</a>
+                                        </li>
+                                        <li><a class="dropdown-item" href="javascript:;">Another action</a>
+                                        </li>
+                                        <li>
+                                            <hr class="dropdown-divider">
+                                        </li>
+                                        <li><a class="dropdown-item" href="javascript:;">Something else here</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <div class="d-flex align-items-center ms-auto font-13 gap-2 mb-3">
+                                <span class="border px-1 rounded cursor-pointer"><i class="bx bxs-circle me-1"
+                                        style="color: #14abef"></i>Branch</span>
+                                <span class="border px-1 rounded cursor-pointer"><i class="bx bxs-circle me-1"
+                                        style="color: #ffc107"></i>Order</span>
+                            </div>
+                            <div class="chart-container-1">
+                                <canvas id="chart1"></canvas>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+
+            </div><!--end row-->
+
+
+            <!--end row-->
+
+            <div class="row row-cols-1 row-cols-lg-3">
+                <div class="col d-flex">
+                    <div class="card radius-10 w-100">
+                        <div class="card-body">
+                            <p class="font-weight-bold mb-1 text-secondary">Weekly Revenue</p>
+                            <div class="d-flex align-items-center mb-4">
+                                <div>
+                                    <h4 class="mb-0">₹0</h4>
+                                </div>
+
+                            </div>
+                            <div class="chart-container-0 mt-5">
+                                <canvas id="chart3"></canvas>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col d-flex">
+                    <div class="card radius-10 w-100">
+                        <div class="card-body">
+                            <p class="font-weight-bold mb-1 text-secondary">Last 5 Month Order</p>
+                            <div class="d-flex align-items-center mb-4">
+                                <div>
+                                    <h4 class="mb-0">56</h4>
+                                </div>
+
+                            </div>
+                            <div class="chart-container-0 mt-5">
+                                <canvas id="chart4"></canvas>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
     </div>
