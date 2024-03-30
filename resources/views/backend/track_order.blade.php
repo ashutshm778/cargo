e64e50f39634b3b01f6a5ddd3e9206eeb9636a89
ashutshm778 <ashutoshm778@gmail.com>
2024-03-30 14:37:13 +0530

    new update

--- resources/views/backend/track_order.blade.php
+++ resources/views/backend/track_order.blade.php
@@ -70,7 +70,7 @@
                                                     </span>
                                                     <span class="fs-12px SFProMedium"><b>Location : </b>
                                                         <activity>
-                                                            @if($log->status == 'order_created')
+                                                            @if($log->status == 'order_created' || $log->status == 'dispatched')
                                                              Branch : {{$log->branch_data->name}},{{$log->branch_data->pincode}},{{$log->branch_data->city}},{{$log->branch_data->state}}
                                                             @endif
                                                         </activity>
