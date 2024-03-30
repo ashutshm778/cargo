cf26990bdb34eec29c162a5de5d3edba26e69de7
ashutshm778 <ashutoshm778@gmail.com>
2024-03-30 15:15:08 +0530

    new update

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -44,7 +44,7 @@ class HomeController extends Controller
 
     public function home()
     {
-        $booking_log = BookingLog::where('user_id', Auth::guard('api')->user()->id)->get();
+        $booking_log = BookingLog::where('user_id', Auth::guard('api')->user()->id)->with('branch_data')->get();
 
         return response()->json([
             'booking_log' => $booking_log,
