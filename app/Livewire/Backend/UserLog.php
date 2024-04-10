c335893455a2e18a00b556f2926e65fbf777821d
ashutshm778 <ashutoshm778@gmail.com>
2024-04-10 10:38:35 +0530

    new update

--- app/Livewire/Backend/UserLog.php
+++ app/Livewire/Backend/UserLog.php
@@ -4,9 +4,15 @@ namespace App\Livewire\Backend;
 
 use Livewire\Component;
 use App\Models\ResourceLog;
+use Livewire\WithPagination;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class UserLog extends Component
 {
+    use AuthorizesRequests;
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
+
     public function render()
     {
         $resource_logs = ResourceLog::where('id', '>', 0);
