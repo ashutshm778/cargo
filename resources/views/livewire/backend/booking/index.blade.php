99c2eed241fbc7f1b971d5da3b248ca285cc306e
ashutshm778 <ashutoshm778@gmail.com>
2024-04-30 14:47:14 +0530

    new update

--- resources/views/livewire/backend/booking/index.blade.php
+++ resources/views/livewire/backend/booking/index.blade.php
@@ -89,7 +89,7 @@
                             </tbody>
                         </table>
                     </div>
-                   2
+                    {{ $bookings->links() }}
                 </div>
             </div>
         </div>
