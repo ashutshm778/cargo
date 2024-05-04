b41607b47a0daa6643be8b0146e57157bbe405b4
ashutshm778 <ashutoshm778@gmail.com>
2024-05-04 18:02:10 +0530

    new update

--- app/Livewire/Backend/Booking/Index.php
+++ app/Livewire/Backend/Booking/Index.php
@@ -6,7 +6,10 @@ use Carbon\Carbon;
 use App\Models\Booking;
 use Livewire\Component;
 use Livewire\WithPagination;
+use App\Exports\BookingExport;
+use Maatwebsite\Excel\Facades\Excel;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
 
 class Index extends Component
 {
@@ -50,5 +53,30 @@ class Index extends Component
         $query = $this->applySearch($bookings);
         $bookings=$query->paginate(10);
         return view('livewire.backend.booking.index',compact('bookings'))->layout('components.backend.layouts.app');
+    }
+
+    public function fileExport()
+    {
+        if(auth()->guard("admin")->user()->id==1){
+            $bookings = Booking::where('id', '>', 0)->with(['branch_from','branch_to'])->orderBy('id','desc');
+        }else{
+            $bookings = Booking::where('id', '>', 0)->where('branch_id',auth()->guard("admin")->user()->branch_id)->with(['branch_from','branch_to'])->orderBy('id','desc');
+        }
+
+        if($this->branch){
+           $bookings=$bookings->where('branch_id',$this->branch);
+        }
+        if($this->startDate && $this->endDate)
+        {
+            $da1=$this->startDate;
+            $da2=$this->endDate;
+            $startDate = Carbon::createFromFormat('Y-m-d', $da1)->startOfDay();
+            $endDate = Carbon::createFromFormat('Y-m-d', $da2)->endOfDay();
+            $bookings = $bookings->whereBetween('created_at', [$startDate, $endDate]);
+        }
+        $query = $this->applySearch($bookings);
+        $bookings=$query->get()->pluck('id');
+
+        return Excel::download(new BookingExport($bookings), 'booking-report.xlsx');
     }
 }
