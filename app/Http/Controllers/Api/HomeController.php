35faa9bcd4ff7c8ab9e0d57e95ebadb7e2398b05
ashutshm778 <ashutoshm778@gmail.com>
2024-05-08 16:31:29 +0530

    new update

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -34,8 +34,8 @@ class HomeController extends Controller
             $data = Admin::where('email', $request->email)->first();
             if (Hash::check($request->password, $data->password)) {
                 $data->access_token =  $data->createToken('MyApp')->plainTextToken;
-                $data->roles = $data->roles;
-                $data->permissions =$data->roles[0]->permissions;
+                $data->roles = $data->roles->first();
+                $data->permissions = $data->roles->first()->permissions;
                 return $data;
             } else {
                 $valid->getMessageBag()->add('password', 'Wrong Password');
