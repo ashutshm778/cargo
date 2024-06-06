591a4f38155df7384a8627c012f17928c05fbb6a
Shailesh Gupta <shaileshhq@gmail.com>
2024-06-06 12:27:02 +0530

    updte

--- resources/views/livewire/backend/booking/show.blade.php
+++ resources/views/livewire/backend/booking/show.blade.php
@@ -2,35 +2,39 @@
     <div class="page-wrapper">
         <div class="page-content" id='printableArea'>
             <!--end breadcrumb-->
-            <div class="table table-bordered" style="border: 1px solid #ddd; background: #fff;">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-12 text-center">
-                            <h4 class="fw-bold mb-1">PRASHANT CARGO & LOGISTICS</h4>
-                            <h6 class="fw-bold mb-0" style="white-space: nowrap;">S-21/123-1, SUBHASH NAGAR MALDAHIYA CANTT,
-                                VARANASI-221005</h6>
-                            <b class="mb-0">Phone :8887790443</b>
-
-                        </div>
-                    </div>
-                </div>
                 <main>
                     <table class="table table-sm table-bordered mt-1 mb-0">
                         <tbody>
                             <tr>
+                                <td colspan="2" style="border-right: 0;">
+                                    <img src="{{ asset('frontend/assets/img/logo.png')}}" alt="img" style="height:75px">
+                                </td>
+                                <td colspan="2" style="border-left: 0;">
+                                    <h4 class="fw-bold mb-1">PRASHANT CARGO & LOGISTICS</h4>
+                                    <h6 class="fw-bold mb-0" style="white-space: nowrap;">S-21/123-1, SUBHASH NAGAR MALDAHIYA
+                                        CANTT,
+                                        VARANASI-221005</h6>
+                                    <b class="mb-0">Phone :8887790443</b>
+                                </td>
+                            </tr>
+                            <tr>
                                 <td style="vertical-align: middle;">AWB No. <b>{{ $booking->bill_no }}</b></td>
-                                <td style="vertical-align: middle;">Date- <b> {{ date('d-m-Y', strtotime($booking->date)) }}</b></td>
-                                <td style="vertical-align: middle;">Destination- <b>{{ $booking->branch_to->name }}</b></td>
-                                <td class="abs text-center"><p class="float-end mb-0"> {!! $barcode !!}
+                                <td style="vertical-align: middle;">Date- <b>
+                                        {{ date('d-m-Y', strtotime($booking->date)) }}</b></td>
+                                <td style="vertical-align: middle;">Destination- <b>{{ $booking->branch_to->name }}</b>
+                                </td>
+                                <td class="abs text-center">
+                                    <p class="float-end mb-0"> {!! $barcode !!}
                                     <p class="mb-0">{{ $booking->bill_no }}</p>
-                                    </p></td>
+                                    </p>
+                                </td>
                             </tr>
                             <tr>
                                 <td colspan="3">From- <b>{{ $booking->branch_from->name }}</b></td>
                                 <td>To- <b>{{ $booking->branch_to->name }}</b></td>
                             </tr>
                             <tr>
-                                <td colspan="4">
+                                <td colspan="4" style="border-bottom: 1px solid transparent;">
                                     <table class="mb-0" style="border:0;width:100%;">
                                         <tbody>
                                             <tr>
@@ -59,9 +63,6 @@
                             </tr>
                         </tbody>
                     </table>
-                    </td>
-                    </tr>
-                    </tbody>
                     </table>
                     <table class="table table-bordered mb-0">
                         <thead>
@@ -93,11 +94,12 @@
                                 </tr>
                             @endforeach
                             <tr class="ftsz">
-                                <td rowspan="6" colspan="6"><p>Seal /Received above mentioned production in good
-                                    condition and correct measure.<br>
-                                    I/We declare that GST shall be payable by consignor/consignee</p>
-                                   <p> I/We have not to claim or avail examption for value
-                                    of goods & material.</p>
+                                <td rowspan="6" colspan="6">
+                                    <p>Seal /Received above mentioned production in good
+                                        condition and correct measure.<br>
+                                        I/We declare that GST shall be payable by consignor/consignee</p>
+                                    <p> I/We have not to claim or avail examption for value
+                                        of goods & material.</p>
                                 </td>
                                 <td colspan="1">Insurance</td>
                                 <td>{{ $booking->insurance }}</td>
@@ -134,16 +136,16 @@
                     <table class="table table-bordered" style="margin-bottom:0;width: 100%;">
                         <tbody>
                             <tr>
-                                <td style="">Consignee's Signature with Ruber Stamp</td>
-                                <td style="text-align: right;"><b style="text-align:left;">PRINCE</b> <br> For Prashant
+                                <td style="border-top: 1px solid transparent;">Consignee's Signature with Ruber Stamp</td>
+                                <td style="text-align: right;border-top: 1px solid transparent;"><b style="text-align:left;">PRINCE</b> <br> For Prashant
                                     Cargo &
                                     Logistics</td>
                             </tr>
                         </tbody>
                     </table>
                 </main>
-            </div>
-            <div class="toolbar no-print mb-3">
+
+            <div class="toolbar no-print mb-3 mt-4">
                 <div class="text-end">
                     <button type="button" id="print_button" class="btn btn-dark"
                         onClick="printDiv('printableArea');"><i class="fa fa-print"></i>
@@ -161,7 +163,5 @@
 
             document.body.innerHTML = originalContents;
         }
-
-
     </script>
 </div>
