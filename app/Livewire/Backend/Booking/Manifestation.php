239856c669ce03e36d18e090dbccadde98316918
ashutshm778 <ashutoshm778@gmail.com>
2024-06-20 14:15:32 +0530

    new update

--- app/Livewire/Backend/Booking/Manifestation.php
+++ app/Livewire/Backend/Booking/Manifestation.php
@@ -111,7 +111,7 @@ class Manifestation extends Component
     public function add_fields(){
         $booking_product_barcode = BookingProductBarcode::where('barcode', $this->awb_no)->first();
         if (!empty($booking_product_barcode->id)) {
-        if(empty(ManifestDetails::where('packet',$booking_product_barcode->barcode)->where('forward_from',auth()->guard("admin")->user()->branch_id)->first()->id)){
+        if(empty(ManifestDetails::where('packet',$booking_product_barcode->barcode)->where('forward_to',auth()->guard("admin")->user()->branch_id)->first()->id)){
 
                 $booking_product = BookingProduct::find($booking_product_barcode->booking_product_id);
                 $booking = Booking::find($booking_product->booking_id)->where('from',auth()->guard("admin")->user()->branch_id)->where('to','!=',auth()->guard("admin")->user()->branch_id)->first();
