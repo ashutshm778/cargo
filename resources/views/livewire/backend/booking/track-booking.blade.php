c7465bff30e6cb4fb2cfaa6a3f94af9e535b8ce8
ashutshm778 <ashutoshm778@gmail.com>
2024-06-20 18:52:25 +0530

    new update

--- resources/views/livewire/backend/booking/track-booking.blade.php
+++ resources/views/livewire/backend/booking/track-booking.blade.php
@@ -9,7 +9,7 @@
                         </div>
                         <div class="ms-auto">
                             @if(!empty($delivery_run_sheet_detail->id))
-                            <a href="{{$delivery_run_sheet_detail->signature}}" target="_blank">Click Here </a>
+                            <a href="{{public_path($delivery_run_sheet_detail->signature)}}" target="_blank">Click Here </a>
                             @endif
                         </div>
                     </div>
